26db5f4fa34871abd5377661cd8e683b973f491d
deltonovel-cell <deltonovel@gmail.com>
2026-07-27 11:21:25 +0200

    Add files via upload
    Added register controller

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace campusMate.Models
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
